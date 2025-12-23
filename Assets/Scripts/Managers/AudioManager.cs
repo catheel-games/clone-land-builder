a4bbe0b545c7d4e6f3c24ed875cd8693d0f11864
nk-oruj <narekorujyan@gmail.com>
2025-12-24 00:50:21 +0400

    - some additions, music playing should be fixed

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,55 +1,168 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : Singleton<AudioManager>
 {
     [Serializable]
-    private struct AudioClipData
+    public struct Sound
     {
         public string name;
         public AudioClip clip;
-        public float pitch;
+        public Vector2 pitch;
     }
 
     [Serializable]
-    private struct AudioGroup
+    public struct Music
     {
         public string name;
-        public AudioClipData[] clips;
+        public AudioClip clip;
     }
 
-    [SerializeField]
-    private AudioGroup[] audioGroups;
-    private Dictionary<string, Dictionary<string, AudioClipData>> audioGroupRegistry;
+    [Serializable]
+    public struct SoundGroup
+    {
+        public string name;
+        public Sound[] sounds;
+    }
+
+    [Serializable]
+    public struct MusicGroup
+    {
+        public string name;
+        public AudioMixerSnapshot snapshot;
+        public Music[] musics;
+    }
+
+    [SerializeField] private AudioSource audioSourcePrefab;
+    [SerializeField] private float musicFadeDuration = 2f;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioMixerGroup soundMixerGroup;
+    [SerializeField] private SoundGroup[] soundGroups;
+
+    [Header("Music")]
+    [SerializeField] private AudioMixerGroup musicMixerGroup;
+    [SerializeField] private MusicGroup[] musicGroups;
+
+
+    private Dictionary<string, Dictionary<string, Sound>> soundRegistry = new Dictionary<string, Dictionary<string, Sound>>();
+    private Dictionary<string, MusicGroup> musicRegistry = new Dictionary<string, MusicGroup>();
+
+    private MusicGroup currentMusicGroup;
+    private AudioSource currentMusicSource;
+    private int currentMusicIndex;
 
     protected override void Awake()
     {
         base.Awake();
 
-        audioGroupRegistry = new Dictionary<string, Dictionary<string, AudioClipData>>();
-
-        foreach (var audioGroup in audioGroups)
+        foreach (SoundGroup group in soundGroups)
         {
-            Dictionary<string, AudioClipData> clipRegistry = new Dictionary<string, AudioClipData>();
-        
-            foreach (var clipData in audioGroup.clips)
+            soundRegistry[group.name] = new Dictionary<string, Sound>();
+
+            foreach (Sound sound in group.sounds)
             {
-                clipRegistry[clipData.name] = clipData;
+                soundRegistry[group.name][sound.name] = sound;
             }
-        
-            audioGroupRegistry[audioGroup.name] = clipRegistry;
+        }
+
+        foreach (MusicGroup group in musicGroups)
+        {
+            musicRegistry[group.name] = group;
         }
     }
 
-    public void Play(string groupName, string clipName)
+    public AudioSource PlaySound(string groupName, string soundName)
     {
-        if (audioGroupRegistry.TryGetValue(groupName, out Dictionary<string, AudioClipData> clipRegistry))
+        if (soundRegistry.TryGetValue(groupName, out var group))
         {
-            if (clipRegistry.TryGetValue(clipName, out AudioClipData clipData))
+            if (group.TryGetValue(soundName, out var sound))
             {
-                AudioSource.PlayClipAtPoint(clipData.clip, Vector3.zero, 1.0f);
+                AudioSource source = Instantiate(audioSourcePrefab, Vector3.zero, Quaternion.identity, transform);
+
+                source.gameObject.name = "Sound Source";
+
+                source.clip = sound.clip;
+                source.outputAudioMixerGroup = soundMixerGroup;
+                source.pitch = UnityEngine.Random.Range(sound.pitch.x, sound.pitch.y);
+
+                source.Play();
+                Destroy(source.gameObject, (sound.clip.length / source.pitch) + 1f);
+
+                return source;
+            }
+            else
+            {
+                Debug.LogWarning($"Sound '{soundName}' not found in group '{groupName}'.");
             }
         }
+        else
+        {
+            Debug.LogWarning($"Sound group '{groupName}' not found.");
+        }
+        
+        return null;
+    }
+
+    public void PlayMusicGroup(string musicQuery)
+    {
+        if (musicRegistry.TryGetValue(musicQuery, out var group))
+        {
+            currentMusicGroup = group;
+            currentMusicIndex = 0;
+            PlayMusic();
+        }
+        else
+        {
+            Debug.LogWarning($"Music query '{musicQuery}' not found.");
+        }
+    }
+
+    private void PlayMusic()
+    {
+        Music music = currentMusicGroup.musics[currentMusicIndex];
+
+        AudioSource newMusicSource = Instantiate(audioSourcePrefab, Vector3.zero, Quaternion.identity, transform);
+
+        newMusicSource.gameObject.name = "Music Source";
+
+        newMusicSource.clip = music.clip;
+        newMusicSource.outputAudioMixerGroup = musicMixerGroup;
+
+        newMusicSource.Play();
+
+        if (currentMusicSource != null)
+        {
+            Sequence sequence = DOTween.Sequence();
+
+            newMusicSource.volume = 0f;
+
+            sequence.Append(currentMusicSource.DOFade(0f, musicFadeDuration));
+            sequence.Join(newMusicSource.DOFade(1f, musicFadeDuration));
+            sequence.OnComplete(() =>
+                {
+                    if (currentMusicSource != null) 
+                    {
+                        if (currentMusicSource.gameObject != null)
+                        {
+                            Destroy(currentMusicSource.gameObject);
+                        }
+                    }
+
+                    currentMusicSource = newMusicSource;
+                }
+            );
+        } 
+        else
+        {
+            newMusicSource.volume = 1f;
+            currentMusicSource = newMusicSource;
+        }
+
+        currentMusicIndex = (currentMusicIndex + 1) % currentMusicGroup.musics.Length;
+        
     }
 }
