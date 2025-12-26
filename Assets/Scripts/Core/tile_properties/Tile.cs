30044fce676bd93f04f52b75f07dd08e31fa6538
nk-oruj <narekorujyan@gmail.com>
2025-12-26 23:55:54 +0400

    - refactored Hayk's code for educational purposes

--- Assets/Scripts/Core/tile_properties/Tile.cs
+++ Assets/Scripts/Core/tile_properties/Tile.cs
@@ -1,81 +1,38 @@
-using System;
 using UnityEngine;
 
+// thank you, Hayk <<3
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private Component[] components;  //start with the North tile component, then go clockwise when uploading start with upper - go to \ then / then _ etc.
-    [SerializeField] private Component centerIdentity;
-    private CircularBuffer buffer;
-    private Component.Types[] types;
-    bool isChosen;  //if Tile was not chosen then it shouldnt rotate, there is a setter function below to change this boolean
+    public enum Type
+    {
+        Null,
+        Town,
+        Grass,
+        Forest,
+        Lake
+    }
 
-    public Tile() {
-        
-        for (int i = 0; i < components.Length; i++)
-        {
-            types[i] = components[i].GetType();
-        }
-        buffer = new CircularBuffer(types);
+    [SerializeField] private Type[] borderTypes;
+    [SerializeField] private Type centerType;
+    
+    private int startIndex;
+    private bool isChosen;
+
+    void Awake() {
+        startIndex = 0;
         isChosen = false;
     }
-    //Tile rotation, just plug the rotation angle from real world, whoever is going to make the actual Tile rotations make sure each rotation step is 60 degrees
-    public void TileRotate(int x) {
-        if (isChosen)
-        {
-            if (x > 0)
-            {
-                buffer.AngleRotateRight(x);
-            }
-            else if (x < 0)
-            {
-                buffer.AngleRotateLeft(x);
-            }
-            else
-            {
 
-                Debug.Log("There was no rotation");
-            }
-
-        }
-        else {
-            Debug.Log("The Tile was not chosen");
-        }
-
-
+    public void Rotate(int step) {
+        startIndex = ((startIndex + step) % 6 + 6) % 6;
+        transform.rotation = Quaternion.AngleAxis(startIndex * 60, Vector3.up);
     }
-    //this function is just a Geo Compass based type getter, it is going to be needed when making the type matching system
-    //for example notice that for the north-east part of the tile the matching part should be another tile's north-west one, and so on
-    //good luck on tile matching system making
-    public Component.Types GeoTileGetter(string compass) { 
-        switch (compass)
-        {
-            case "N":
-                return buffer.Get(0); //gets the type of North Tile
 
-            case "NE":
-                return buffer.Get(1); // North East Tile Type
-
-            case "SE":
-                return buffer.Get(2);
-
-            case "S":
-                return buffer.Get(3);
-
-            case "SW":
-                return buffer.Get(4);
-
-            case "NW":
-                return buffer.Get(5);
-            default:
-                print("Oops, something went wrong with your GeoTileGetter, recheck the input argument");
-                return Component.Types.Null;
-        }
+    public Type GetSideType(int side) { 
+        return borderTypes[(startIndex + side) % 6];
     }
 
     public void ChooseTile() { 
         isChosen = true;    
     }
-
-
-
 }
