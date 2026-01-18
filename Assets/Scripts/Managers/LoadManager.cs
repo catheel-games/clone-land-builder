ca9b63b8c546385dfef287016a3fa6b06f371a6f
Dvso <dkdallakyan@gmail.com>
2026-01-18 05:07:52 +0400

    - loading screen slider changes, fake loading implementation, title & slider scaling up, dots

--- Assets/Scripts/Managers/LoadManager.cs
+++ Assets/Scripts/Managers/LoadManager.cs
@@ -19,18 +19,48 @@ public class LoadManager : Singleton<LoadManager>
     IEnumerator LoadSceneCoroutine(string sceneName)
     {
         loadingScreen.Show();
-        
+        loadingScreen.SetProgress(0);
+        loadingScreen.TitleScaling();
+
         operation = SceneManager.LoadSceneAsync(sceneName);
         operation.allowSceneActivation = false;
 
-        do
-        {
-            loadingScreen.SetProgress(Mathf.Clamp01(operation.progress / 0.9f));
-            yield return new WaitForSeconds(0.1f);
-        }
-        while (operation.progress < 0.9f);
+        float minLoadTime = 1.25f;
+        float elapsedTime = 0f;
 
-        yield return new WaitForSeconds(0.6f);
+        float displayedProgress = 0f;
+        float fakeSpeed = 1.5f;
+
+        float stopPercent = Random.Range(0.87f, 0.93f);
+        int randomDisplaying;
+
+        while (operation.progress < 0.9f || elapsedTime < minLoadTime)
+        {
+            float realProgress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            float targetProgress = Mathf.Min(realProgress, stopPercent);
+
+            displayedProgress = Mathf.MoveTowards (displayedProgress, targetProgress, fakeSpeed * Time.deltaTime);
+
+            randomDisplaying = Random.Range(0, 7);
+            if (randomDisplaying == 0 || displayedProgress >= stopPercent)
+                loadingScreen.SetProgress(Mathf.Round(displayedProgress * 100f));
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.15f);
+
+        while (displayedProgress < 1f)
+        {
+            displayedProgress = Mathf.MoveTowards (displayedProgress, 1f, fakeSpeed * Time.deltaTime);
+
+            loadingScreen.SetProgress(Mathf.Round(displayedProgress * 100f));
+            yield return null;
+        }
+
+        loadingScreen.StopAllCoroutines();
 
         operation.allowSceneActivation = true;
         operation = null;
