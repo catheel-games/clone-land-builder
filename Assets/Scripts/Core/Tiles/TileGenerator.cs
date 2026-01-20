b39688b1d1e65342fb28fb755cc7fb222689e416
karmirarev <maritamovsisyan@gmail.com>
2026-01-21 00:20:17 +0400

    - created the tile containers (type and full), started implementing set generator

--- Assets/Scripts/Core/Tiles/TileGenerator.cs
+++ Assets/Scripts/Core/Tiles/TileGenerator.cs
@@ -24,14 +24,14 @@ public class TileGenerator : Singleton<TileGenerator>
     }
 
     [SerializeField] private Tile[] tiles;
+    [SerializeField] private FullTiles fullTiles;
+    [SerializeField] private TypeTiles typeTiles; 
 
-    List<Tile> displayedTiles = new List<Tile>();
+    List<Tile> set = new List<Tile>();
 
     void Start()
     {
-        displayedTiles.Add(GetRandomTile());
-        displayedTiles.Add(GetRandomTile());
-        displayedTiles.Add(GetRandomTile());
+        GenerateStartingSets();
     }
 
     private Tile GetRandomTile()
@@ -40,17 +40,102 @@ public class TileGenerator : Singleton<TileGenerator>
         return tiles[randomNum];
     }
 
+    private Tile.Type GetRandomType()
+    {
+        return (Tile.Type)UnityEngine.Random.Range(1, 6);
+    } 
+
+    private Tile.Type GetRandomTypeExcluding(Tile.Type firstType)
+    {
+        Tile.Type secondType = GetRandomType();
+        if (secondType == firstType)
+            return GetRandomTypeExcluding(firstType);
+        return secondType;
+    }
+
+    private Tile GetFullTileOfType(Tile.Type type)
+    {
+        Tile[] arr;
+        switch (type)
+        {
+            case Tile.Type.Grass:
+                arr = fullTiles.fullGrassTiles;
+                break;
+            case Tile.Type.Water:
+                arr = fullTiles.fullWaterTiles;
+                break;
+            case Tile.Type.Town:
+                arr = fullTiles.fullTownTiles;
+                break;
+            case Tile.Type.Yellow:
+                arr = fullTiles.fullYellowTiles;
+                break;
+            case Tile.Type.Forest:
+                arr = fullTiles.fullForestTiles;
+                break;
+            default: 
+                arr = fullTiles.fullGrassTiles;
+                break;
+        }
+
+        return arr[UnityEngine.Random.Range(0, arr.Length)];
+    }
+
+    private Tile GetTypeTileOfType(Tile.Type type)
+    {
+        Tile[] arr;
+        switch (type)
+        {
+            case Tile.Type.Grass:
+                arr = typeTiles.grassTiles;
+                break;
+            case Tile.Type.Water:
+                arr = typeTiles.waterTiles;
+                break;
+            case Tile.Type.Town:
+                arr = typeTiles.townTiles;
+                break;
+            case Tile.Type.Yellow:
+                arr = typeTiles.yellowTiles;
+                break;
+            case Tile.Type.Forest:
+                arr = typeTiles.forestTiles;
+                break;
+            default:
+                arr = typeTiles.grassTiles;
+                break;
+        }
+
+        return arr[UnityEngine.Random.Range(0, arr.Length)];
+    }
+
+    private List<Tile> GenerateSet(Tile.Type type)
+    {
+        List<Tile> newSet = new List<Tile>();
+        newSet.Add(GetFullTileOfType(type));
+        newSet.Add(GetTypeTileOfType(type));
+        newSet.Add(GetTypeTileOfType(type));
+        return newSet;
+    }
+
+    private void GenerateStartingSets()
+    {
+        Tile.Type type1 = GetRandomType();
+        Tile.Type type2 = GetRandomTypeExcluding(type1);
+        set.AddRange(GenerateSet(type1));
+        set.AddRange(GenerateSet(type2));
+        Debug.Log("Queue has " + set.Count + " tiles!");
+    }
+
     public Tile ShowNextTile()
     {
-        return displayedTiles[0];
+        return set[0];
     }
 
     public Tile GetNextTile()
     {
-        Tile prevTile = displayedTiles[0];
-        displayedTiles.RemoveAt(0);
-        displayedTiles.Add(GetRandomTile());
-
+        Tile prevTile = set[0];
+        set.RemoveAt(0);
         return prevTile;
     }
 }
