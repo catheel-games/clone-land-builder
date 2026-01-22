9f51e7721c7cd9ed6093b6b166aab41079a72ba4
Marita <maritamovsisyan@gmail.com>
2026-01-22 17:04:30 +0400

    - minor display ui fixes

--- Assets/Scripts/Core/Tiles/TileQueueUI.cs
+++ Assets/Scripts/Core/Tiles/TileQueueUI.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using DG.Tweening;
 using System.Collections.Generic;
 
 public class TileQueueUI : Singleton<TileQueueUI>
 {
     [SerializeField] private Transform[] tileSlots;
+    [SerializeField] private GameObject whitePlatePrefab;
     private List<GameObject> displayedTiles = new List<GameObject>();
 
     void Start()
@@ -23,6 +25,15 @@ public class TileQueueUI : Singleton<TileQueueUI>
 
         for (int i = 0; i < tileSlots.Length && i < queue.Count; i++)
         {
+            GameObject whitePlate = Instantiate(
+                whitePlatePrefab,
+                tileSlots[i].position + new Vector3(0, -0.1f, 0),
+                Quaternion.identity
+            );
+            whitePlate.transform.localScale = new Vector3(1.1f, 1f, 1f);
+            whitePlate.layer = 3;
+            displayedTiles.Add(whitePlate);
+
             GameObject newTile = Instantiate(
                 queue[i].gameObject,
                 tileSlots[i].position,
@@ -30,6 +41,17 @@ public class TileQueueUI : Singleton<TileQueueUI>
             );
             newTile.layer = 3;
             displayedTiles.Add(newTile);
+
+            if (i == 0)
+            {
+                newTile.transform.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.1f); 
+                whitePlate.transform.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.1f);
+            }
+            else
+            {
+                newTile.transform.DOScale(new Vector3(1f, 1f, 1f), 0.1f);
+                whitePlate.transform.DOScale(new Vector3(1f, 1f, 1f), 0.1f);
+            }
         }
     }
 }
