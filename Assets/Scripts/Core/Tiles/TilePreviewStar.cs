7419a23474afe056bf6673b34dd15901f5f5edde
nk-oruj <narekorujyan@gmail.com>
2026-01-28 23:16:16 +0400

    - star scale in feedback done

--- Assets/Scripts/Core/Tiles/TilePreviewStar.cs
+++ Assets/Scripts/Core/Tiles/TilePreviewStar.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class TilePreviewStar : MonoBehaviour
 {
     [SerializeField] private float rotationCoefficient = 40f;
+    [SerializeField] private TilePreviewStarScaleInFeedback scaleInFeedback;
+
+    void Start()
+    {
+        scaleInFeedback.Play();
+    }
 
     void Update()
     {
---
+++ Assets/Scripts/Feedbacks/TilePreviewStarScaleInFeedback.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class TilePreviewStarScaleInFeedback : MonoBehaviour
+{
+    [SerializeField] private float duration = 0.6f;
+    [SerializeField] private float maxScale = 1.8f;
+
+    public void Play()
+    {
+        Sequence scaleInSequence = DOTween.Sequence();
+
+        transform.localScale = Vector3.zero;
+
+        scaleInSequence.Append(transform.DOScale(maxScale, duration * 0.8f));
+        scaleInSequence.Append(transform.DOScale(Vector3.one, duration * 0.2f));
+    }
+}
