a4943d391c0545de9960c3d546201c450152f62a
Dvso <dkdallakyan@gmail.com>
2026-01-29 06:07:27 +0400

    - collected stars flying to HUD done, sounds added, overall done

--- Assets/Scripts/Feedbacks/StarCollecting.cs
+++ Assets/Scripts/Feedbacks/StarCollecting.cs
@@ -7,15 +7,32 @@ using DG.Tweening;
 public class StarCollecting : MonoBehaviour
 {
     [SerializeField] private Transform starCircleTransform;
+    [SerializeField] private Transform tileCounterTransform;
     [SerializeField] private RectTransform starScoreTransform;
     [SerializeField] private TextMeshProUGUI starText1;
     [SerializeField] private TextMeshProUGUI starText2;
     [SerializeField] private TextMeshProUGUI tileText;
 
+    [Header("Instantiating Prefabs")]
+    [SerializeField] private RectTransform tileFlyPrefab;
+    [SerializeField] private RectTransform starFlyPrefab;
+    [SerializeField] private Transform canvasTransform;
+
+    [Header("Targets")]
+    [SerializeField] private RectTransform starTarget;
+    [SerializeField] private RectTransform tileTarget;
+
+    [Header("Star Plus Sounds")]
+    [SerializeField] private string[] starPlusesSounds;
+    private string currentPlusSound;
+
     private int starAmount = 0;
     private int tileAmount = 0;
-    private List<GameObject> starObjects = new List<GameObject>();
+    private List<GameObject> starObjects = new();
     private GameObject tileObject;
+
+    private List<RectTransform> spawnedStars = new();
+    private List<RectTransform> spawnedTiles = new();
 
     public void SetMaximumStars() {
         //Set starText2 to LevelData Max
@@ -30,36 +47,111 @@ public class StarCollecting : MonoBehaviour
 
     public void SetAcceptFeedback()
     {
+        currentPlusSound = starPlusesSounds[Random.Range(0, starPlusesSounds.Length)];
+
+        spawnedStars.Clear();
+        spawnedTiles.Clear();
+
         LevelControl.Instance.tileScore--;
         tileText.text = LevelControl.Instance.tileScore.ToString();
+        tileText.transform.DOScale(1.2f, 0.1f).SetEase(Ease.OutQuad).SetLoops(2, LoopType.Yoyo);
 
-        if (tileObject != null) {
-            switch (starAmount) {
-                case 3:
-                    tileAmount = 1; break;
-                case 4:
-                    tileAmount = 2; break;
-                case 5:
-                    tileAmount = 3; break;
-                case 6:
-                    tileAmount = 6; break;
+        if (starAmount != 0)
+        {
+
+            if (tileObject != null)
+            {
+                switch (starAmount)
+                {
+                    case 3:
+                        tileAmount = 1; break;
+                    case 4:
+                        tileAmount = 2; break;
+                    case 5:
+                        tileAmount = 3; break;
+                    case 6:
+                        tileAmount = 6; break;
+                }
+
+                for (int i = 0; i < tileAmount; i++)
+                {
+                    Vector3 screenPos = Camera.main.WorldToScreenPoint(tileObject.transform.position);
+                    SpawnTileFly(screenPos);
+                }
+                Destroy(tileObject);
             }
+
+            else if (tileObject == null)
+                tileAmount = 0;
+
+            for (int i = 0; i < starObjects.Count; i++)
+            {
+                Vector3 screenPos = Camera.main.WorldToScreenPoint(starObjects[i].transform.position);
+                SpawnStarFly(screenPos);
+                Destroy(starObjects[i]);
+            }
+
+            //-------------------------------------------------------- SEQUENCES ------------------------------------------------------------------------------------------
+
+            Sequence starsSeq = DOTween.Sequence();
+
+            Sequence innerStarsSeq = DOTween.Sequence();
+            for (int i = 0; i < spawnedStars.Count; i++)
+            {
+                int index = i;
+                innerStarsSeq.AppendCallback(() =>
+                {
+                    AnimateToSlot(spawnedStars[index], starTarget, false);
+                });
+
+                innerStarsSeq.AppendInterval(0.07f);
+            }
+
+            starsSeq.Append(innerStarsSeq)
+                    .AppendCallback(() => AudioManager.Instance.PlaySound("Plus", currentPlusSound))
+                    .AppendInterval(0.125f)
+                    .Join(starCircleTransform.DOScale(1.25f, 0.1f).SetEase(Ease.OutQuad).SetLoops(2, LoopType.Yoyo))
+                    .AppendCallback(() => SetScore(LevelControl.Instance.starScore, starAmount, starText1))
+                    .AppendCallback(() => LevelControl.Instance.starScore += starAmount);
+
+
+            starsSeq.Pause();
+
+            Sequence tilesSeq = DOTween.Sequence();
+
+            Sequence innerTilesSeq = DOTween.Sequence();
+            for (int i = 0; i < spawnedTiles.Count; i++)
+            {
+                int index = i;
+                innerTilesSeq.AppendCallback(() =>
+                {
+                    AnimateToSlot(spawnedTiles[index], tileTarget, true);
+                });
+
+                innerTilesSeq.AppendInterval(0.05f);
+
+            }
+
+            tilesSeq.Append(innerTilesSeq)
+                    //.AppendCallback(() => AudioManager.Instance.PlaySound("Plus", "Tile Plus"))
+                    .AppendInterval(0.1f)
+                    .Join(tileCounterTransform.DOScale(1.25f, 0.1f).SetEase(Ease.OutQuad).SetLoops(2, LoopType.Yoyo))
+                    .AppendCallback(() => SetScore(LevelControl.Instance.tileScore, tileAmount, tileText))
+                    .AppendCallback(() => LevelControl.Instance.tileScore += tileAmount);
+
+            tilesSeq.Pause();
+
+            Sequence wholeSeq = DOTween.Sequence();
+
+            wholeSeq.Insert(0f, starScoreTransform.DOAnchorPosX(0, 0.1f))
+                    .Insert(0.1f, starsSeq);
+
+            if (tileAmount != 0) {
+                wholeSeq.Insert(0.45f, tilesSeq);
+            }
+
+
         }
-        else if (tileObject == null)
-            tileAmount = 0;
-
-
-        Sequence feedbackSeq = DOTween.Sequence();
-
-        feedbackSeq.Join(starScoreTransform.DOAnchorPosX(0, 0.1f))
-                   .Append(starCircleTransform.DOScale(1.25f, 0.2f))
-
-                   .AppendCallback(() => SetScore(LevelControl.Instance.tileScore, tileAmount, tileText))
-                   .AppendCallback(() => SetScore(LevelControl.Instance.starScore, starAmount, starText1))
-                   .AppendCallback(() => LevelControl.Instance.tileScore += tileAmount)
-                   .AppendCallback(() => LevelControl.Instance.starScore += starAmount)
-
-                   .Append(starCircleTransform.DOScale(1f, 0.2f));
     }
 
     private void SetScore(int lastAmount, int plusAmount, TextMeshProUGUI text)
@@ -84,4 +176,47 @@ public class StarCollecting : MonoBehaviour
             }
         }
     }
+
+    private void SpawnStarFly(Vector3 screenPos)
+    {
+        RectTransform fly = Instantiate(starFlyPrefab, canvasTransform);
+        fly.position = screenPos;
+
+        spawnedStars.Add(fly);
+
+        fly.localScale = Vector3.one * 0.8f;
+        fly.DOMoveY(100f, 0.075f).SetRelative();
+    }
+
+    private void SpawnTileFly(Vector3 screenPos)
+    {
+        RectTransform fly = Instantiate(tileFlyPrefab, canvasTransform);
+
+        Vector3 randomOffset = new Vector3(
+            Random.Range(-50f, 50f),
+            Random.Range(-50f, 50f),
+            0f                          );
+
+        fly.position = screenPos + randomOffset;
+
+        spawnedTiles.Add(fly);
+
+        fly.localScale = Vector3.one * 0.8f;
+    }
+
+    private void AnimateToSlot(RectTransform fly, RectTransform target, bool withVoice)
+    {
+        fly.DOScale(1f, 0.3f).SetEase(Ease.OutBack);
+        fly.DOMove(target.position, 0.35f)
+            .SetEase(Ease.OutCubic)
+            .OnComplete(() =>
+            {
+                if (withVoice)
+                    AudioManager.Instance.PlaySound("Plus", "Tile Plus");
+
+                VibrationManager.Instance.Vibrate();
+                Destroy(fly.gameObject);
+            });
+    }
+
 }
--- Assets/Scripts/Feedbacks/TilePlacementFeedback.cs
+++ Assets/Scripts/Feedbacks/TilePlacementFeedback.cs
@@ -38,8 +38,6 @@ public class TilePlacementFeedback : MonoBehaviour
         StopCoroutine(hoveringCoroutine);
         hoveringCoroutine = null;
         currentTileTransform.DOKill();
-
-        Debug.Log("Hovering Stopped");
     }
 
     IEnumerator Hovering()
