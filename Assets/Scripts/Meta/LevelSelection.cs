8cdaa0522fb2a4f5918e03eb34465c177c7547e0
Dvso <dkdallakyan@gmail.com>
2026-01-29 20:54:32 +0400

    - level selection based on level data, level roads & level button colors, in progress icons based on level data, changed gon icons

--- Assets/Scripts/Meta/LevelSelection.cs
+++ Assets/Scripts/Meta/LevelSelection.cs
@@ -1,19 +1,121 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelSelection : MonoBehaviour
 {
     [SerializeField] private UIContainer levelSelection;
     [SerializeField] private UIContainer completedLevelSelection;
 
+    [SerializeField] private LevelSelectionPanel levelSelectionPanel;
+
+    [SerializeField] private Animator[] levelAnimators;
+    [SerializeField] private Image[] levelImages;
+    [SerializeField] private TextMeshProUGUI[] levelNumberTexts;
+
+    [SerializeField] private GameObject[] levelSimpleRoads;
+    [SerializeField] private GameObject[] levelDottedRoads;
+    [SerializeField] private GameObject[] levelInProgressObjects;
+
+    [SerializeField] private Sprite greenSprite;
+    [SerializeField] private Sprite blueSprite;
+    [SerializeField] private Sprite greySprite;
+    [SerializeField] private Color whiteColor;
+    [SerializeField] private Color greenColor;
+
+
+    private LevelDataSO levelData;
+    private LevelProgress progress;
+
+    private void Start()
+    {
+        levelSelectionPanel = levelSelection.gameObject.GetComponent<LevelSelectionPanel>();
+
+        SetLevelButtons();
+    }
+
+    public void SetLevelButtons()
+    {
+        for (int i = 0; i < levelImages.Length; i++)
+        {
+            LevelProgress progress = GameData.Instance.GetLevelProgress(i+1);
+
+            LevelProgress.LevelState _state = progress != null ? progress.state : LevelProgress.LevelState.Blocked;
+
+            levelInProgressObjects[i].SetActive(false);
+
+            if (i != 0)
+            {
+                levelSimpleRoads[i-1].SetActive(true);
+                levelDottedRoads[i-1].SetActive(false);
+            }
+
+            switch (_state) {
+                case LevelProgress.LevelState.Blocked:
+                    levelImages[i].sprite = greySprite;
+                    levelAnimators[i].enabled = false;
+                    levelNumberTexts[i].color = whiteColor;
+                    if (i != 0) {
+                        levelSimpleRoads[i-1].SetActive(false);
+                        levelDottedRoads[i-1].SetActive(true);
+                    }
+                    break;
+
+                case LevelProgress.LevelState.Unlocked:
+                    levelImages[i].sprite = blueSprite;
+                    levelAnimators[i].enabled = true;
+                    levelNumberTexts[i].color = whiteColor;
+                    break;
+                case LevelProgress.LevelState.InProgress:
+                    levelImages[i].sprite = blueSprite;
+                    levelAnimators[i].enabled = true;
+                    levelNumberTexts[i].color = whiteColor;
+                    levelInProgressObjects[i].SetActive(true);
+                    break;
+                case LevelProgress.LevelState.Finished:
+                    levelImages[i].sprite = greenSprite;
+                    levelAnimators[i].enabled = false;
+                    levelNumberTexts[i].color = greenColor;
+                    break;
+            }
+
+        }
+    }
+
     public void MenuSelectLevel(int level)
     {
-        if (Random.Range(0, 2) == 0)
+        levelData = SaveLoadManager.Instance.levelDatas[level];
+
+        progress = GameData.Instance.GetLevelProgress(levelData.levelIndex);
+
+        if (progress == null)
         {
-            levelSelection.Show();
+            progress = new LevelProgress
+            {
+                levelIndex = levelData.levelIndex,
+                currentScore = 0,
+                currentTilesLeft = 50,
+                state = LevelProgress.LevelState.Blocked
+            };
+
+            GameData.Instance.levelsProgress.Add(progress);
         }
-        else
+
+        if (progress.state == LevelProgress.LevelState.Finished)
         {
             completedLevelSelection.Show();
+
         }
+        else if (progress.state == LevelProgress.LevelState.Blocked)
+        {
+            Debug.Log("This level is Blocked");
+        }
+        else {
+            levelSelection.Show();
+
+            bool played = progress.state == LevelProgress.LevelState.InProgress;
+            levelSelectionPanel.SetValues(levelData.goalValue, levelData.coinValue, played, levelData.levelTiles);
+        }
+
     }
 }
